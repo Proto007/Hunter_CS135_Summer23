a6e54331b7eee1a54bdee26c76eae4e80bcbc513
Sadab Hafiz <sadabhafizny@gmail.com>
2023-06-07 19:36:12 -0400

    update deadlines

--- course-app/src/deadlinesdata.js
+++ course-app/src/deadlinesdata.js
@@ -1,10 +1,5 @@
 export const deadlinesData = [
     {
-        name: "Lab 2",
-        deadline : "06/07/2023 11:00 AM",
-        type : "lab"
-    },
-    {
         name: "Homework 1",
         deadline : "06/11/2023 11:59 PM",
         type : "classwork/homework"

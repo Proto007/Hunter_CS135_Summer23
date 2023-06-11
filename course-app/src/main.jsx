7c77a38a39ede8cf76705a66e59fd0195dda58fb
Sadab Hafiz <sadabhafizny@gmail.com>
2023-06-11 00:19:28 -0400

    try another fix to gh pages deployment

--- course-app/src/main.jsx
+++ course-app/src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Hunter_CS135_Summer23">
       <App/>
     </BrowserRouter>
   </React.StrictMode>,

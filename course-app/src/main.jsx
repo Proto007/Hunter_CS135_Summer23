96005c219c60599b430c4a4583436a8073fb045d
Sadab Hafiz <sadabhafizny@gmail.com>
2023-06-11 22:23:47 -0400

    use HashRouter instead of BrowserRouter

--- course-app/src/main.jsx
+++ course-app/src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <HashRouter>
+      <App/>
+    </HashRouter>
   </React.StrictMode>,
 )

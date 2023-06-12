84bc742aad9822409ff0d51779481e338561f239
Sadab Hafiz <sadabhafizny@gmail.com>
2023-06-11 21:38:44 -0400

    set up react router routes

--- course-app/src/App.jsx
+++ course-app/src/App.jsx
@@ -1,30 +1,13 @@
+import Homepage from './components/Homepage';
 import './index.css'
-import Navbar from './components/Navbar'
-import Schedule from './components/Schedule'
-import Faq from './components/Faq'
-import Info from './components/Info'
-import { faqData } from './faqData'
-import { deadlinesData } from './deadlinesData'
-import { homeworksData } from './homeworksData'
+import { Routes, Route } from 'react-router-dom';
+import Project1 from './project/project_1/Project1';
 
 export default function App() {
   return (
-    <div>
-      <Navbar homeworks={homeworksData}/>
-      <div className=" flex flex-col justify-center items-center">
-        <Info assignments={ deadlinesData }/>
-        <div className="border-solid border border-black w-10/12 mt-5 p-5 text-xl font-mono text-center font-bold">Class Schedule</div>
-        <Schedule/>
-        <div className="border-solid border-2 border-black bg-black w-10/12 mt-10 p-5 text-xl text-white font-mono font-bold">Frequently Asked Questions</div>
-        <div className="w-10/12">
-          {faqData.map((section, index) => (
-            <Faq key={index} section={section}/>
-          ))}
-        </div>
-      </div>
-      <footer className='bg-gray-700 text-white text-center w-full font-mono p-5 mt-10'>
-        This website was made using  and <a className="text-blue-400" href="https://react.dev/">ReactJS</a> and <a className="text-blue-400" href='https://tailwindcss.com/'>Tailwind-CSS</a>.
-      </footer>
-    </div>
+    <Routes>
+      <Route path="Hunter_CS135_Summer23" element={<Homepage/>}/>
+      <Route path="Hunter_CS135_Summer23/Project_1" element={<Project1/>}/>
+    </Routes> 
   )
 }
---
+++ course-app/src/components/Homepage.jsx
@@ -0,0 +1,29 @@
+import Navbar from './Navbar'
+import Schedule from './Schedule'
+import Faq from './Faq'
+import Info from './Info'
+import { faqData } from '../faqData'
+import { deadlinesData } from '../deadlinesData'
+import { homeworksData } from '../homeworksData'
+
+export default function Homepage(){
+    return (
+        <div>
+            <Navbar homeworks={homeworksData}/>
+            <div className=" flex flex-col justify-center items-center">
+                <Info assignments={ deadlinesData }/>
+                <div className="border-solid border border-black w-10/12 mt-5 p-5 text-xl font-mono text-center font-bold">Class Schedule</div>
+                <Schedule/>
+                <div className="border-solid border-2 border-black bg-black w-10/12 mt-10 p-5 text-xl text-white font-mono font-bold">Frequently Asked Questions</div>
+                <div className="w-10/12">
+                {faqData.map((section, index) => (
+                    <Faq key={index} section={section}/>
+                ))}
+                </div>
+            </div>
+            <footer className='bg-gray-700 text-white text-center w-full font-mono p-5 mt-10'>
+                This website was made using  and <a className="text-blue-400" href="https://react.dev/">ReactJS</a> and <a className="text-blue-400" href='https://tailwindcss.com/'>Tailwind-CSS</a>.
+            </footer>
+        </div>
+    )
+}

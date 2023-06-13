e3f5d0cd4909f3c9c5bd3fc0bbeb24d2d78f37e4
Sadab Hafiz <sadabhafizny@gmail.com>
2023-06-13 19:12:45 -0400

    fix formatting

--- course-app/src/components/Schedule.jsx
+++ course-app/src/components/Schedule.jsx
@@ -25,7 +25,7 @@ export default function Schedule(){
                         <a target="_blank" href="https://maryash.github.io/135/slides/1.7%20PS%20Algorithm%20Design.pdf">Algorithm Design</a><br/>
                         <a target="_blank" href="https://maryash.github.io/135/slides/2.1%20Variables.pdf">Variables</a>
                     </td>
-                    <td className="border border-slate-700 p-0 text-center text-blue-700 font-bold">
+                    <td className="border border-slate-700 p-1 text-center text-blue-700 font-bold">
                         <a target="_blank" href="https://maryash.github.io/135/labs/lab_01.html">Lab 1</a><br/>
                         <a target="_blank" href="/Hunter_CS135_Summer23/recitation_slides/Lab_1.pdf">Slides</a><br/>
                     </td>
@@ -46,7 +46,7 @@ export default function Schedule(){
                         <a target="_blank" href="https://maryash.github.io/135/slides/3.2%20Comparing%20Numbers%20and%20Strings.pdf">Comparing Numbers and Strings</a><br/>
                         <a target="_blank" href="https://maryash.github.io/135/slides/3.3%20Multiple%20Alternatives%20_%203.4%20Nested%20Branches.pdf">Multiple Alternatives</a>
                     </td>
-                    <td className="border border-slate-700 p-0 text-center text-blue-700 font-bold">
+                    <td className="border border-slate-700 p-1 text-center text-blue-700 font-bold">
                         <a target="_blank" href="https://maryash.github.io/135/labs/lab_02.html">Lab 2</a><br/>
                         <a target="_blank" href="/Hunter_CS135_Summer23/recitation_slides/Lab_2.pdf">Slides</a><br/>
                     </td>
@@ -67,7 +67,7 @@ export default function Schedule(){
                         <a target="_blank" href="https://maryash.github.io/135/slides/4.4%20The%20DO%20Loop%20_%204.5%20Processing%20Input.pdf">Do-While Loops</a><br/>
                         <a target="_blank" href="https://maryash.github.io/135/slides/4.6%20PS%20Storyboards%20_%204.7%20Common%20Loop%20Algorithms%20_%204.8%20Nested%20Loops.pdf">Loop Algorithms</a>
                     </td>
-                    <td className="border border-slate-700 p-0 text-center text-blue-700 font-bold">
+                    <td className="border border-slate-700 p-1 text-center text-blue-700 font-bold">
                         <a target="_blank" href="https://maryash.github.io/135/labs/lab_03.html">Lab 3</a><br/>
                         <a target="_blank" href="/Hunter_CS135_Summer23/extra_content/Current_Reservoir_Levels.tsv">Datafile</a><br/>
                         <a target="_blank" href="/Hunter_CS135_Summer23/recitation_slides/Lab_3.pdf">Slides</a><br/>
@@ -87,7 +87,7 @@ export default function Schedule(){
                         <a target="_blank" href="https://maryash.github.io/135/slides/5.4%20Return%20Values%20_%205.5%20Functions%20without%20Return%20Values%20_%205.6%20Reusable%20Functions.pdf">Returning from a function</a><br/>
 
                     </td>
-                    <td className="border border-slate-700 p-0 text-center text-blue-700 font-bold">
+                    <td className="border border-slate-700 p-1 text-center text-blue-700 font-bold">
                         <a target="_blank" href="https://maryash.github.io/135/labs/lab_04.html">Lab 4</a><br/>
                         <a target="_blank" href="/Hunter_CS135_Summer23/recitation_slides/Lab_4.pdf">Slides</a><br/>
                     </td>
@@ -105,7 +105,7 @@ export default function Schedule(){
                         <a target="_blank" href="https://maryash.github.io/135/slides/Static%20Variables.pdf">Static Variables</a><br/>
                         <a target="_blank" href="https://maryash.github.io/135/slides/5.9%20Reference%20Parameters.pdf">Reference Parameters</a><br/>
                     </td>
-                    <td className="border border-slate-700 p-0 text-center text-blue-700 font-bold">
+                    <td className="border border-slate-700 p-1 text-center text-blue-700 font-bold">
                         <a target="_blank" href="/#/Project_1">Project 1</a><br/>
                     </td>
                 </tr>

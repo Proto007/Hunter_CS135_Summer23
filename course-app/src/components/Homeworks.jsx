eaf55ba1ca6c139ed71438cbea8446043b510131
Sadab Hafiz <sadabhafizny@gmail.com>
2023-07-18 11:43:13 -0400

    add scrollbar for homeworks

--- course-app/src/components/Homeworks.jsx
+++ course-app/src/components/Homeworks.jsx
@@ -29,7 +29,7 @@ export default function Homeworks(homeworks) {
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items className="absolute z-10 mt-2 w-full text-center text-sm p-1 font-mono origin-top-right divide-y divide-gray-100 rounded-sm bg-white shadow-lg ring-1 ring-black ring-opacity-10 focus:outline-none">
+          <Menu.Items className="absolute z-10 mt-2 w-full text-center text-sm p-1 font-mono origin-top-right divide-y divide-gray-100 rounded-sm bg-white shadow-lg ring-1 ring-black ring-opacity-10 focus:outline-none h-52 overflow-auto">
             {
               homeworks.homeworks.map((week, weekIndex)=>{
                 return (

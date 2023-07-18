57495ba713035dea5726977dd9d5655eb92db764
Sadab Hafiz <sadabhafizny@gmail.com>
2023-07-18 11:46:21 -0400

    add final exam to deadlines

--- course-app/src/deadlinesdata.js
+++ course-app/src/deadlinesdata.js
@@ -1,15 +1,5 @@
 export const deadlinesData = [
     {
-        name: "Homework 6",
-        deadline : "07/16/2023 11:59 PM",
-        type : "classwork/homework"
-    },
-    {
-        name: "Lab 11",
-        deadline: "07/17/2023 11:00 AM",
-        type: "lab"
-    },
-    {
         name: "Lab 12",
         deadline: "07/19/2023 11:00 AM",
         type: "lab"
@@ -33,6 +23,11 @@ export const deadlinesData = [
         name: "Project 2 Task B",
         deadline : "07/24/2023 11:00 AM",
         type: "project"
+    },
+    {
+        name: "FINAL EXAM",
+        deadline: "07/26/2023 11:40 AM",
+        type: "exam"
     },
     {
         name: "Project 3 - Extra Credit",

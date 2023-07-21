212ab56dd553ba503d24761674c9d6326e7d7de2
Sadab Hafiz <sadabhafizny@gmail.com>
2023-07-21 15:19:35 -0400

    add teacher evaluations deadline

--- course-app/src/deadlinesdata.js
+++ course-app/src/deadlinesdata.js
@@ -20,6 +20,11 @@ export const deadlinesData = [
         type: "exam"
     },
     {
+        name: "Teacher Evaluations",
+        deadline: "07/26/2023 11:59 PM",
+        type: "exam"
+    },
+    {
         name: "Project 3 - Extra Credit",
         deadline : "07/26/2023 11:59 PM",
         type: "project"

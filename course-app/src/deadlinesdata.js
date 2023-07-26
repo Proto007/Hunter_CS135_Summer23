d6cbb13050e732b14810d280a43f03436c6bcbcc
Sadab Hafiz <sadabhafizny@gmail.com>
2023-07-26 19:42:42 -0400

    update deadlines

--- course-app/src/deadlinesdata.js
+++ course-app/src/deadlinesdata.js
@@ -1,15 +1,5 @@
 export const deadlinesData = [
     {
-        name: "FINAL EXAM",
-        deadline: "07/26/2023 11:40 AM",
-        type: "exam"
-    },
-    {
-        name: "Teacher Evaluations",
-        deadline: "07/26/2023 11:59 PM",
-        type: "exam"
-    },
-    {
         name: "Project 3 - Extra Credit",
         deadline : "07/26/2023 11:59 PM",
         type: "project"
